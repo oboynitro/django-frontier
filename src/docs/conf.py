6ec15c0e13be829db9bc298f77be3d54adc1e51a
Frank Owusu Boakye <oboy.nitro1@gmail.com>
2020-11-16 06:49:39 +0000

    updated docs

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -10,7 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import sphinx_glpi_theme
+# import sphinx_glpi_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
