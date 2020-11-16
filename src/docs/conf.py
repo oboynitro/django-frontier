200fc18e7ddbf1c2ccd18df4b76fc61736650505
Frank Owusu Boakye <oboy.nitro1@gmail.com>
2020-11-16 06:54:14 +0000

    updated docs

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -32,6 +32,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_glpi_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
