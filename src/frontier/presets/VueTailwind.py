9a36c7f1727492e913bdb9aa04524807c88dd71a
Frank Owusu Boakye <oboy.nitro1@gmail.com>
2020-11-17 10:13:44 +0000

    completed vue with tailwindcss scaffolding

---
+++ src/frontier/presets/VueTailwind.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import shutil
+
+
+class VueTailwind():
+    def install(self, resource_path, base_dir):
+        self.base_dir = base_dir
+        self.resource_path = resource_path
+        self.components_dir = Path.joinpath(self.resource_path, "js/")
+
+        self.components_source = Path.joinpath(
+            Path(__file__).resolve().parent, "samples/vue_tailwind/src")
+
+        self.components_config = Path.joinpath(
+            Path(__file__).resolve().parent, "samples/vue_tailwind/config")
+
+        shutil.copytree(str(self.components_source), str(self.components_dir))
+        for file in self.components_config.glob("*"):
+            shutil.copy2(str(file), str(self.base_dir))
